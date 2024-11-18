766820a0f7a9a317c900fef09db4a94b7b5d882a
RyKaT07 <patrykski07@gmail.com>
2024-11-18 12:43:41 +0100

    fix: added credentials

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="google-api-lib",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "google-api-python-client>=2.0.0",
+        "google-cloud-secret-manager>=2.0.0",
+        "requests>=2.0.0",
+    ],
+    description="Utilities for working with Google services.",
+    author="Patryk Skibniewski",
+    author_email="patrykski07@gmail.com",
+    url="https://github.com/RyKaT07/google-api-lib",  # Zastąp swoim repozytorium
+)
