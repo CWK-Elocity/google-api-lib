a160611b57f4f355d5d17275beb29cd8b308ab90
RyKaT07 <patrykski07@gmail.com>
2024-11-18 13:22:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="google_api_lib",
+    name="google-api-lib",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
