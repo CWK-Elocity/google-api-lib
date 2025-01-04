f464558ca973864cb11224bb070538adb233468b
RyKaT07 <patrykski07@gmail.com>
2025-01-04 15:04:33 +0100

    fix: added to init

--- google_api_lib/__init__.py
+++ google_api_lib/__init__.py
@@ -1,3 +1,3 @@
 from .auth import authenticate_with_cloud
 from .drive import DriveFile
-from .secrets import access_secret, save_secret
+from .secrets import access_secret, save_secret, delete_secret_version, save_and_cleanup_secret
