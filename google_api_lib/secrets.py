5feb84f5df009c5f79979777c562d340a98cd94f
RyKaT07 <patrykski07@gmail.com>
2025-01-04 15:30:28 +0100

    feat: added debug version

--- google_api_lib/secrets.py
+++ google_api_lib/secrets.py
@@ -96,3 +96,45 @@ def save_and_cleanup_secret(secret_id, data, project_id=None):
     except Exception as e:
         print(f"Error during secret update or cleanup: {e}")
         raise
+
+def save_and_cleanup_secret_debug(secret_id, data, project_id=None):
+    if project_id is None:
+        project_id = get_project_metadata("project-id")
+        if project_id is None:
+            return ValueError("Project ID is required.")
+
+    try:
+        # Zapisz nową wersję sekretu
+        save_secret(secret_id=secret_id, data=data, project_id=project_id)
+
+        print("Fetching versions before cleanup:")
+        # Pobierz wszystkie wersje sekretu, aby znaleźć starszą wersję
+        client = secretmanager.SecretManagerServiceClient()
+        parent = f"projects/{project_id}/secrets/{secret_id}"
+        versions = client.list_secret_versions(parent=parent)
+        
+        for version in versions:
+            print(f"Version: {version.name}, State: {version.state.name}")
+        
+        # Znajdź najnowszą wersję i wersję starszą o jeden
+        versions_sorted = sorted(
+            (v for v in versions if v.state.name == "ENABLED"),
+            key=lambda v: int(v.name.split('/')[-1]),
+            reverse=True
+        )
+
+        if len(versions_sorted) > 1:
+            previous_version = versions_sorted[1].name.split('/')[-1]
+
+            # Usuń starszą wersję
+            delete_secret_version(secret_id=secret_id, version_id=previous_version, project_id=project_id)
+            print("Fetching versions after cleanup:")
+            versions_after = client.list_secret_versions(parent=parent)
+            for version in versions_after:
+                print(f"Version: {version.name}, State: {version.state.name}")
+        else:
+            print("No older secret version to delete.")
+
+    except Exception as e:
+        print(f"Error during secret update or cleanup: {e}")
+        raise
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="google-api-lib",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     install_requires=[
         "google-api-python-client>=2.0.0",
